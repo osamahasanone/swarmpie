83815cedbf5531937863f49a1a5994511e529231
Osama <osamahasanone@gmail.com>
2021-07-21 09:06:00 +0300

    refactor nmea tests

--- swarm/tests/test_nmea.py
+++ swarm/tests/test_nmea.py
@@ -4,41 +4,51 @@ from ..nmea import ChecksumCalculator, NMEAMessageComposer, NMEAMessageChecker
 
 class TestChecksumCalculator:
 
-    def test_falsy_sentence(self):
+    @pytest.mark.parametrize('sentence', [None, '$any', 'any$any', 'any$'])
+    def test_init_fail(self, sentence):
         with pytest.raises(ValueError):
-            m = ChecksumCalculator(sentence=None)
-        with pytest.raises(ValueError):
-            m = ChecksumCalculator(sentence="any$any")
+            cc = ChecksumCalculator(sentence)
 
-    @pytest.fixture
-    def checksum_calc(self):
-        return ChecksumCalculator(sentence='DT 300')
+    def test_init_success(self):
+        sentence = 'any'
+        cc = ChecksumCalculator(sentence)
+        assert cc.sentence == sentence
 
-    def test_unicode(self, checksum_calc):
-        assert checksum_calc.unicode() == [68, 84, 32, 51, 48, 48]
+    @pytest.fixture(scope='class')
+    def dummy_checksum_calc(self):
+        return ChecksumCalculator('DT 300')
 
-    def test_xor(self, checksum_calc):
-        assert checksum_calc.xor() == 3
+    def test_unicode(self, dummy_checksum_calc):
+        assert dummy_checksum_calc.unicode() == [68, 84, 32, 51, 48, 48]
 
-    def test_checksum(self, checksum_calc):
-        return checksum_calc.checksum == '03'
+    def test_xor(self, dummy_checksum_calc):
+        assert dummy_checksum_calc.xor() == 3
+
+    @pytest.mark.parametrize('sentence,result', [('FV', '10'), ('DT 300', '03'), ('GN 30', '2a')])
+    def test_checksum(self, sentence, result):
+        cc = ChecksumCalculator(sentence)
+        assert cc.checksum == result
 
 
 class TestNMEAMessageComposer:
 
-    @pytest.fixture
-    def message(self):
-        return NMEAMessageComposer(sentence='DT 300')
-
-    def test_str(self, message):
-        assert str(message) == '$DT 300*03'
+    def test_str(self):
+        nmea_mc = NMEAMessageComposer(sentence='DT 300')
+        assert str(nmea_mc) == '$DT 300*03'
 
 
 class TestNMEAMessageChecker:
+
+    def test_init_success(self):
+        nmea_c = NMEAMessageChecker(sentence='FV', received_checksum=10)
+        assert nmea_c.sentence == 'FV'
+        assert nmea_c.received_checksum == 10
 
     @pytest.fixture
     def message(self):
         return NMEAMessageChecker(sentence='DT 300', received_checksum='03')
 
-    def test_is_valid(self, message):
-        assert message.is_valid() == True
+    @pytest.mark.parametrize('sentence,received_checksum,result', [('DT 300', '03', True), ('FV', '09', False)])
+    def test_is_valid(self, sentence, received_checksum, result):
+        nmea_c = NMEAMessageChecker(sentence, received_checksum)
+        assert nmea_c.is_valid() == result
