a1092a3d5c30d71a8b59b74ce1c390cea2184a72
Osama <osamahasanone@gmail.com>
2021-07-27 09:06:32 +0300

    add models to admin site

--- server/command/admin.py
+++ server/command/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class VerbParameterAdmin(admin.ModelAdmin):
+    fields = ('verb', 'order')
+    list_display = ('id', 'verb', 'order')
+    ordering = ['id']
+
+
+class VerbParameterChoiceAdmin(admin.ModelAdmin):
+    fields = ('verb_parameter', 'header', 'value', 'description')
+    list_display = ('id', 'verb_parameter', 'header', 'value', 'description')
+    ordering = ['id']
+
+
+class CommandAdmin(admin.ModelAdmin):
+    fields = ('verb',)
+    list_display = ('id', 'verb', 'ts')
+    ordering = ['id']
+
+
+class CommandParameterAdmin(admin.ModelAdmin):
+    fields = ('command', 'verb_parameter', 'value')
+    list_display = ('id', 'command', 'verb_parameter', 'value')
+    ordering = ['id']
+
+
+admin.site.register(VerbParameter, VerbParameterAdmin)
+admin.site.register(VerbParameterChoice, VerbParameterChoiceAdmin)
+admin.site.register(Command, CommandAdmin)
+admin.site.register(CommandParameter, CommandParameterAdmin)
--- server/common/admin.py
+++ server/common/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class VerbAdmin(admin.ModelAdmin):
+    fields = ('name', 'description')
+    list_display = ('id', 'name', 'description')
+    ordering = ['id']
+
+
+admin.site.register(Verb, VerbAdmin)
