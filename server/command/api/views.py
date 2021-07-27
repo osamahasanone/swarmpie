146b5df42d35f95c070b918904405246c67ad681
Osama <osamahasanone@gmail.com>
2021-07-27 11:17:07 +0300

    avoid mixin in verb parameter view

--- server/command/api/views.py
+++ server/command/api/views.py
@@ -1,12 +1,8 @@
 from command.models import VerbParameter
 from .serializers import VerbParameterSerializer
-from rest_framework import mixins
 from rest_framework import generics
 
 
-class VerbParameterList(mixins.ListModelMixin, generics.GenericAPIView):
+class VerbParameterList(generics.ListAPIView):
     queryset = VerbParameter.objects.all()
     serializer_class = VerbParameterSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
