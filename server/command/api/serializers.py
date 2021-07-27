ae06a6a8d656b7139c08dd9476c69b4b5fde216a
Osama <osamahasanone@gmail.com>
2021-07-27 12:16:51 +0300

    remove command from command parameter serializer

--- server/command/api/serializers.py
+++ server/command/api/serializers.py
@@ -17,14 +17,20 @@ class VerbParameterSerializer(serializers.ModelSerializer):
 
 
 class CommandParameterSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = CommandParameter
-        fields = ('id', 'command', 'verb_parameter', 'value')
+        fields = ('id', 'verb_parameter', 'value')
+        depth = 1
 
 
 class CommandSerializer(serializers.ModelSerializer):
+    verb = serializers.SerializerMethodField()
     parameters = CommandParameterSerializer(many=True, read_only=True)
 
     class Meta:
         model = Command
         fields = ('id', 'verb', 'ts', 'parameters')
+
+    def get_verb(self, command):
+        return command.verb.name
