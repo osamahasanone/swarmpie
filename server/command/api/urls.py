a59c55a8a9ec1cb87ee58124f29bd6bd764bf4c5
Osama <osamahasanone@gmail.com>
2021-07-27 11:46:42 +0300

    remove retrieve url for verb parameter

--- server/command/api/urls.py
+++ server/command/api/urls.py
@@ -4,8 +4,6 @@ from .views import *
 urlpatterns = [
     path('verb_parameters', VerbParameterList.as_view(),
          name='verb_parameters_list'),
-    path('verb_parameters/<int:pk>', VerbParameterList.as_view(),
-         name='verb_parameter_retrieve'),
     path('command_parameters', CommandParameterList.as_view(),
          name='command_parameters_list'),
     path('commands', CommandList.as_view(), name='commands_list'),
